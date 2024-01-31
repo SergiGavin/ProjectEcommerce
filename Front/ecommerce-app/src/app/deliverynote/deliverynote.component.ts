13cc857d46c553dfb34410c5ce19fbc599e3a594
Sergi Gavin <sergi.gs@hotmail.es>
2024-01-31 13:23:45 +0100

    Inicio front pagina principal diseño y creacion componentes

---
+++ Front/ecommerce-app/src/app/deliverynote/deliverynote.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deliverynote',
+  standalone: true,
+  imports: [],
+  templateUrl: './deliverynote.component.html',
+  styleUrl: './deliverynote.component.css'
+})
+export class DeliverynoteComponent {
+
+}
